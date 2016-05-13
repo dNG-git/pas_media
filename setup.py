aa28ebf10672419a38627dd2a513592d93a7cc6d
NotTheEvilOne <git-NTEO@vplace.de>
2016-05-13 11:58:07 +0200

    Trivial: Added "platforms" list to "setup.py" Trivial: Added "et al." to the authors attribute in "setup.py"

--- setup.py
+++ setup.py
@@ -62,10 +62,12 @@ with TemporaryDirectory(dir = ".") as build_directory:
 	      version = get_version(),
 	      description = "Python Application Services",
 	      long_description = """"pas_media" provides the abstract classes to query metadata of multimedia files.""",
-	      author = "direct Netware Group",
+	      author = "direct Netware Group et al.",
 	      author_email = "web@direct-netware.de",
 	      license = "GPLv2+",
 	      url = "https://www.direct-netware.de/redirect?pas;media",
+
+	      platforms = [ "any" ],
 
 	      package_dir = { "": _build_path },
 	      packages = [ "dNG" ],
